c4c3dbf9a681abd6618338d4de8e7f904ffe4c23
AKSHAT PANKAJ VISHWAKARMA <akshatvishwakarma259@gmail.com>
2026-07-16 20:40:15 +0530

    ✅ Print Function | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-16

--- hackerrank/medium/python-print/solution.py
+++ hackerrank/medium/python-print/solution.py
@@ -1,10 +1,4 @@
-def is_leap(year):
-    if year % 400 == 0:
-        return True
-    elif year % 100 == 0:
-        return False
-    elif year % 4 == 0:
-        return True
-    else:
-        return False
-
+n=int(input())
+k=n+1
+for i in range(1,k):
+    print(i,end="")
