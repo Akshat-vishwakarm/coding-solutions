68f0c68c56d99e8a413b9bd36f417f394eb8efa7
AKSHAT PANKAJ VISHWAKARMA <akshatvishwakarma259@gmail.com>
2026-09-14 15:52:51 +0530

    ✅ Palindrome Number | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 10.8 MB | 2026-09-14

---
+++ leetcode/easy/0009-palindrome-number/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+
+        int first = 0;
+        int last = s.length() - 1;
+
+        while (first < last) {
+            if (s[first] != s[last]) {
+                return false;
+            }
+
+            first++;
+            last--;
+        }
+
+        return true;
+    }
+};
